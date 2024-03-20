82c2bdb78d8c51d116a4d2091d4165a64bf3340e
Al-Baraa Ibrahim Melhem <121182529+Baraa-Melhem@users.noreply.github.com>
2024-03-20 14:00:17 +0300

    Add files via upload

--- DataPrepKit.py
+++ DataPrepKit.py
@@ -2,8 +2,9 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 class DataProcessor:
+    #Reads data
     def read_data(path):
-# Reading data 
+
         if path.lower().endswith('.csv'):
             data = pd.read_csv(path)
         elif path.lower().endswith('.xlsx'):
@@ -11,28 +12,32 @@ class DataProcessor:
         elif path.lower().endswith('.json'):
             data = pd.read_json(path)
         else:
-            raise ValueError("Unsupported data file format")
+            raise ValueError("unsupported data file")
 
         return data
-#summarizing data
+
+    #data summary
     def summarize_data(data):
         return data.describe()
-#handle with missing data
+
+    #Handles missing values
     def handle_missing_values(data):
 
         data.dropna(inplace=True)
         data.fillna(data.mean(), inplace=True)
-# encoding categorical columns
+
+    #Encoding categorical columns
     def encode_categorical_columns(data):
+
         label_encoder = LabelEncoder()
         for column in data.select_dtypes(include=['object']).columns:
             data[column] = label_encoder.fit_transform(data[column])
 
         return data
 
-# to try the code :
+#example to try the code:
 """
-file_path = "data.csv"  
+file_path = "your_data.csv"  # Replace with the actual file path
 processor = DataProcessor()
 data = processor.read_data(file_path)
 summary = processor.summarize_data(data)
@@ -40,9 +45,11 @@ print(summary)
 processor.handle_missing_values(data)
 encoded_data = processor.encode_categorical_columns(data)
 print(encoded_data)
-"""
 
 """
-here is one of my projects that I applied data processing on the data to use it to train my AI model:
+
+
+"""
+here is one of my projects that I applied data processing on the data to use AI model:
 https://www.kaggle.com/code/baraamelhem/titaniccomp 
 """
